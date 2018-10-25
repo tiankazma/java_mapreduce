971d54bc67a1a20df11938ffc70c699011386dd6
tiankazma <tian_liu_1201@163.com>
2018-10-25 09:18:18 +0800

    create mapreduce project
    create the maven project of mapreduce practice

---
+++ my_mapreduce/src/main/java/tian/bigdata/my_mapreduce/App.java
@@ -0,0 +1,13 @@
+package tian.bigdata.my_mapreduce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
